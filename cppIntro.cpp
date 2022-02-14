f008dab6a8469b859633034844d5157bb1f0a00f
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-02-14 21:11:55 +0100

    new files

---
+++ cppIntro.cpp
@@ -0,0 +1,15 @@
+//
+//  cppIntro.cpp
+//  
+//
+//  Created by Joseph Muchengeti on 14.02.22.
+//
+
+#include "cppIntro.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::cin.get();
+}
---
+++ cppIntro.hpp
@@ -0,0 +1,13 @@
+//
+//  cppIntro.hpp
+//  
+//
+//  Created by Joseph Muchengeti on 14.02.22.
+//
+
+#ifndef cppIntro_hpp
+#define cppIntro_hpp
+
+#include <stdio.h>
+
+#endif /* cppIntro_hpp */
