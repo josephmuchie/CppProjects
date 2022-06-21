c6180ad56dbdf22c7db3e37965e448f535a603c0
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-06-22 01:33:16 +0200

    added ignorer, preorder and postorder traversal

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -23,6 +23,33 @@ public:
     }
 };
 
+void preorder(BinaryTree* root){
+    if(root == nullptr){
+        return;
+    }
+    cout<< root->value <<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(BinaryTree* root){
+    if(root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->value<< " ";
+    inorder(root->right);
+}
+
+void postorder(BinaryTree* root){
+    if(root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    inorder(root->right);
+    cout<<root->value <<" ";
+}
+
 void calculateBranchSums(BinaryTree *node, int runningSum, vector<int> &sums) {
     if(node == nullptr)
         return;
@@ -50,7 +77,15 @@ int main(int argc, const char * argv[]) {
     root->right = new BinaryTree(3);
     root->left->left = new BinaryTree(4);
     root->left->right = new BinaryTree(5);
-    branchSums(root);
+    root->right->left = new BinaryTree(6);
+    root->right->right = new BinaryTree(7);
     
+    branchSums(root);
+    preorder(root);
+    cout<<" "<<endl;
+    inorder(root);
+    cout<<" "<<endl;
+    postorder(root);
+    cout<<" "<<endl;
     return 0;
 }
