c675068a81ecf7bce92d9230ffb9a1b76b2c4d86
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-06-21 19:44:47 +0200

    initial commit

---
+++ DataStructures/DataStructures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Joseph Muchengeti on 21.06.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
