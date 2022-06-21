091094fc9c285fc7c30ae3568ffe2862862bd14e
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-06-22 00:40:40 +0200

    binary trees init

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -6,9 +6,32 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BinaryTree{
+public:
+    int value;
+    BinaryTree *left;
+    BinaryTree *right;
+    
+    BinaryTree(int value){
+        this->value = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    BinaryTree *root = new BinaryTree(1);
+    root->left = new BinaryTree(2);
+    root->right = new BinaryTree(3);
+    root->left->left = new BinaryTree(4);
+    root->left->right = new BinaryTree(5);
+    //branchSums(root);
+    
     return 0;
 }
