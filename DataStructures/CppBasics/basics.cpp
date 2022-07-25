49be212a58e086c2d933ed52f09d1db1c29b68bd
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-07-25 18:44:34 +0200

    tamplates in cpp

--- DataStructures/CppBasics/basics.cpp
+++ DataStructures/CppBasics/basics.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <iterator>
 #include <array>
+#include <random>  //for std:: mt19937 and std:: uniform_int_distribution
 
 using namespace std;
 
@@ -48,9 +49,9 @@ public:
 }
 };
 
+template <typename POW> 
 
-
-std::int64_t powint(int base, int exp)
+std::int64_t powint(POW base, POW exp)
 {
     assert(exp >= 0 && "powint: exp param has negative value");
     
@@ -74,6 +75,10 @@ void print_container(const std::vector<int>& c)
 
 int main()
 {
+    std:: mt19937 mt; //Instantiate a 32-bit Mersenne Twister
+    std:: uniform_int_distribution die6(1,6);  //generate random numbers in a range 1-6
+
+
     string arr[]{"Plumtree","Bulawayo","Mt Pleasant","Matshobana", "Luveve"};
     int arr_length = sizeof(arr)/sizeof(arr[0]);
 
@@ -98,5 +103,14 @@ int main()
     char myString[]{"Muchie"};
     const int length{ static_cast<int>(std::size(myString))};
     Print(myString);
+
+    for(int count{1}; count <= 40; ++count)
+    {
+        std::cout << die6(mt) << '\t';
+
+        if(count % 10 == 0)  //If we've printed 10 numbers, start a new row
+        std::cout << '\n';
+    }
+
     return 0;
 }
