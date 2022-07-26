40063c9476562b22f4302b8a93efc7d5816bf747
Joseph Muchengeti <muchengetijoseph@gmail.com>
2022-07-26 15:41:24 +0200

    Constructors and destructors

--- DataStructures/CppBasics/maths.cpp
+++ DataStructures/CppBasics/maths.cpp
@@ -15,6 +15,25 @@ class Cars{
       double price;
    
    public: 
+      Cars(){
+         cout<< "Default constructor called .."<<endl;
+      }
+
+      Cars(string mName, string fType,float m, double p ){
+         cout<< "Parameterized constructor called .."<<endl;
+         modelName = mName;
+         fuelType = fType;
+         mileage = m;
+         price = p;
+      }
+      Cars(Cars &obj){
+         cout<< "Copy constructor called .."<<endl;
+         companyName = obj.companyName;
+         modelName = obj.modelName;
+         fuelType = obj.fuelType;
+         mileage = obj.mileage;
+         price = obj.price;
+      }
       void setData(string cName, string mName, string fType,float m, double p){
          companyName = cName;
          modelName = mName;
@@ -29,12 +48,21 @@ class Cars{
          cout <<"Mileage - "<< mileage<<endl;
          cout <<"Price - "<< price<<endl;
       }
+      ~Cars(){
+         cout<< "Destructor called .."<<endl;
+      }
 };
 
 int main()
 {
-   Cars car1;  //Car object 
+   Cars car1,car2("F1 Car","Petrol",34.7,5850);  //Car object 
    car1.setData("AMG ","Petronas","Diesel",123667.23,30000);
    car1.displayData();
+   cout<<" " <<endl;
+   car2.displayData();
+   cout<<" "<<endl;
+
+   Cars car3 = car1;
+   car3.displayData();
    return 0;
 }
